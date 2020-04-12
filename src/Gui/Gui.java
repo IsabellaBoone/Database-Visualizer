9d9ceeb00d2155a8d62b77910b5cb0c6a3971bf3
Chase <cb5522@cs.ship.edu>
2020-04-12 12:07:27 -0400

    Just Testing

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -3,7 +3,11 @@ package Gui;
 import javax.swing.*;
 
 public class Gui {
+  JFrame jf;
   public Gui() {
-    
+    jf = new JFrame();
+    jf.setSize(1000, 750);
+    jf.setVisible(true);
+    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import Gui.*;
 public class Runner {
 
   public static void main(String[] args) {
-
+    Gui gui = new Gui();
   }
 
 }
