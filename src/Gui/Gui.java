c30131434d742397be6cc47916fcfb3c63527fd2
Chase <cb5522@cs.ship.edu>
2020-04-12 12:55:01 -0400

    started a little gui stuff

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -1,13 +1,27 @@
 package Gui;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class Gui {
+  
   JFrame jf;
+  Container one;
+  JLabel items = new JLabel();
+  
   public Gui() {
     jf = new JFrame();
+    jf.setBackground(Color.BLACK);
     jf.setSize(1000, 750);
-    jf.setVisible(true);
+    jf.setLayout(new GridBagLayout());
+    jf.setResizable(false);
     jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jf.setVisible(true);
+    drawLayout();
   }
+  
+  private void drawLayout() {
+    
+  }
+  
 }
