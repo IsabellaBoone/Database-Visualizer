f82e0c8b03f791426e984c0298ee0a1c77625a47
@jj7253 <jj7253@cs.ship.edu>
2020-04-30 11:56:13 -0400

    chase is boat

--- src/Gui/PlayersLayout.java
+++ src/Gui/PlayersLayout.java
@@ -3,30 +3,30 @@ package Gui;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
 import database.RetrieveManipulateInformation;
 
 import database.RetrieveManipulateInformation;
 
+//Author: Joshua J
 public class PlayersLayout extends JPanel{
 	 private RetrieveManipulateInformation rmi;// used for replacing manipulating information ya know
-	  private JLabel playerJLabel[], charactersJLabel[], // hold every single label in characters n players
-	  
-	  selectedPlayerJLabel, // Needed for highlighting selected player
-	  selectedCharacterJLabel; // Needed for highlighting selected character
-	  
+	  private JLabel playerJLabel[], charactersJLabel[], selectedPlayerJLabel, selectedCharacterJLabel; // Needed for highlighting selected character
 	  private JPanel curCharactersPanel = null, curStatsPanel = null; // used for replacing panels, kinda lowkey important 
 	  private GridBagConstraints charPanel, statPanel; // used for replacing panels, kinda lowkey important 
 	  private String selectedPlayerUsername = null, selectedCharacterName = null; // maybe not needed
-	  
 	  private String[] playerUsernames, charNames;
 	  
 	  public PlayersLayout(RetrieveManipulateInformation rmi) {
@@ -61,74 +61,26 @@ public class PlayersLayout extends JPanel{
 	  }
 	  
 	  private JPanel genUsernamePanel() {
-	    JPanel panel = new JPanel(); 
+	    JPanel panel = new JPanel();
+	    panel.setBackground(new Color(80, 80, 80));
+	    panel.setLayout(new BorderLayout());
+	    Dimension d = new Dimension(300, 600);
+	    panel.setMinimumSize(d);
+	    panel.setPreferredSize(d);
+	    panel.setMaximumSize(d);
 	    
-	    panel.setBackground(Color.DARK_GRAY);
-	    panel.setLayout(new GridBagLayout()); 
+	    JScrollPane items = new JScrollPane();
+	    items.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	    items.add(items.createVerticalScrollBar());
+
+	    JPanel locations = new JPanel();
+	    locations.setBackground(new Color(132, 132, 132));
+	    locations.setLayout(new GridBagLayout());
+
+	    playerJLabel = new JLabel[rmi.getNumLocations()];
 	    
-	    playerJLabel = new JLabel[rmi.getNumPlayers()];
-	    playerUsernames = rmi.getAllPlayerUsernames();
 	    
-	    GridBagConstraints c = new GridBagConstraints(); 
-	    c.gridx = 0;
-	    c.gridy = 0; 
-	    c.weightx = 0.95;
-	    c.ipady = 20; 
-	    panel.add(new JLabel("<html><H1 Style = \"color:#808080; font-size: 30px\">" + "Players:" + "</H1></html>"));
-	    
-	    c.gridy++; 
-	    
-	    // Add all usernames
-	    for(int i = 0; i < playerUsernames.length; i++) {
-	      // HTML Formatting for JLabel, then add mouse listener.
-	      String format = "<html><body style = \"color:white; font-size: 22px\">" + playerUsernames[i] + "</body></html>"; // text with html formatting
-	      playerJLabel[i] = new JLabel(format, SwingConstants.CENTER);
-	      playerJLabel[i].addMouseListener(new MouseAdapter() {
-	        @Override
-	        public void mouseClicked(MouseEvent e) {
-	          // Remove background from selected player
-	          if(selectedPlayerJLabel == null) {
-	            // tee hee ya know
-	          } else {
-	            removeSelectedBackgroundPlayer(); 
-	          }
-	          
-	          // Find what label the user selected
-	          for(int j = 0; j < playerJLabel.length; j++) {
-	            if(e.getSource() == playerJLabel[j]) {
-	              /*
-	               * When we find what player was selected, 
-	               * change playerSelected variable to keep 
-	               * track for future references, and change
-	               * the background color to yellow.
-	               * 
-	               * Then, break out because there is no point to
-	               * the loop anymore. 
-	               */
-	              selectedPlayerJLabel = playerJLabel[j]; 
-	              selectedPlayerUsername = playerUsernames[j]; // Store the string for later
-	              playerJLabel[j].setOpaque(true);
-	              playerJLabel[j].setBackground(new Color(234, 201, 55));
-	              break;
-	            }
-	          }
-	          if(curCharactersPanel == null) {
-	            JPanel pp = genCharacterPanel(); 
-	            curCharactersPanel = pp; 
-	            add(pp, charPanel);
-	            pp.validate();
-	            pp.repaint();
-	          } else {
-	            refreshCharacterPanel(); 
-	          }
-	        }
-	      });
-	    
-	      panel.add(playerJLabel[i], c);
-	      c.gridy++; 
-	    }
-	    
-	    return panel; 
+      return panel; 
 	  }
 	  
 	  /**
